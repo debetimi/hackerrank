2a9a10b9f5460fcfceecd2612b28f1bd39925fa3
Debetimi Okah <debetimi@gmail.com>
2014-06-18 01:24:31 -0700

    stopped being an idiot

--- src/hackerrank/filling_jars.clj
+++ src/hackerrank/filling_jars.clj
@@ -9,9 +9,10 @@
 
 (defn -main 
   []
-  (let [[n, m] (parse) 
-        sums (vec (repeat n 0))]
-    (loop [sums sums i m]
-      (if (zero? i) (println (int (/ (reduce + sums) (count sums)))) 
+  (let [[n, m] (parse)]
+    (loop [sums 0 i m]
+      (if (zero? i) 
+        (println (/ sums n)) 
         (let [[a b k] (parse)]
-          (recur (reduce #(update-in %1 [%2] + k) sums (range (dec a) b)) (dec i)))))))
+          (recur (+ sums (* k (inc (- b a)))) (dec i)))))))
+
